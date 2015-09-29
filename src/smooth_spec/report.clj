d0dcbf30aa257970fd80dba0f060bff569a9e31a
Jeff Slavin <jdslavin@thenavisway.com>
2015-09-29 14:09:02 -0700

    RPC-208 - Added reporting data structures to enable color coding of test data and be able to roll those colors up the test specification - Reorganized async spec to flow a little better.

--- src/smooth_spec/report.clj
+++ src/smooth_spec/report.clj
@@ -1,11 +1,13 @@
 (ns smooth-spec.report
     (:require [clojure.test :as t :refer (are is deftest with-test run-tests testing testing-vars-str)]
                [clojure.stacktrace :as stack]
+               [smooth-spec.report-data :as rd]
                )
      (:import clojure.lang.ExceptionInfo)
   )
 
 (def ^:dynamic *test-level* (atom 0))
+
 
 (defn space-level []
   (apply str (repeat (* 3 @*test-level*) " "))
@@ -46,6 +48,7 @@
 (defmethod smooth-report :pass [m]
   (t/with-test-out
     (t/inc-report-counter :pass)
+    (rd/pass)
     ))
 
 (defmethod smooth-report :error [m]
@@ -53,6 +56,12 @@
     (t/inc-report-counter :error)
     (print-smooth-fail (t/testing-vars-str m))
     (print-diagnostics m)
+    (let [detail {:where    (clojure.test/testing-vars-str m)
+                    :message  (:message m)
+                    :expected (str (:expected m))
+                    :actual   (str (:actual m))}]
+      (rd/error detail)
+      )
     ))
 
 (defmethod smooth-report :fail [m]
@@ -63,23 +72,32 @@
     (println "expected:" (pr-str (:expected m)))
     (println "  actual:" (pr-str (:actual m)))
     (println)
+    (let [detail {:where    (clojure.test/testing-vars-str m)
+                  :message  (:message m)
+                  :expected (str (:expected m))
+                  :actual   (str (:actual m))}]
+      (rd/fail detail)
+      )
     ))
 
 (defmethod smooth-report :begin-test-ns [m]
   (t/with-test-out
-    (println "\nTesting" (ns-name (:ns m)))
-    ))
+    (println "\nTesting" (ns-name (:ns m))))
+  (rd/begin-namespace (ns-name (:ns m)))
+  )
 
 (defmethod smooth-report :begin-specification [m]
   (t/with-test-out
     (reset! *test-level* 0)
     (println (space-level) (:string m)))
-  )
+  (rd/begin-specification (:string m)))
+
 
 (defmethod smooth-report :end-specification [m]
   (t/with-test-out
     (println)
     (reset! *test-level* 0)
+    (rd/end-specification)
     )
   )
 
@@ -87,24 +105,28 @@
   (t/with-test-out
     (swap! *test-level* inc)
     (println (space-level) (:string m)))
+    (rd/begin-behavior (:string m))
   )
 
 (defmethod smooth-report :end-behavior [m]
   (t/with-test-out
     (swap! *test-level* dec)
     )
+  (rd/end-behavior)
   )
 
 (defmethod smooth-report :begin-provided [m]
   (t/with-test-out
     (swap! *test-level* inc)
     (println (space-level) (:string m)))
+  (rd/begin-provided (:string m))
   )
 
 (defmethod smooth-report :end-provided [m]
   (t/with-test-out
     (reset! *test-level* 0)
     )
+  (rd/end-provided)
   )
 
 (defmethod smooth-report :summary [m]
@@ -112,6 +134,9 @@
     (println "\nRan" (:test m) "tests containing"
              (+ (:pass m) (:fail m) (:error m)) "assertions.")
     (println (:fail m) "failures," (:error m) "errors.")
+    (let [stats {:passed (:pass m) :failed (:fail m) :error (:error m)}]
+      (rd/summary stats)
+      )
     )
   )
 
