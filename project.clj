e1516d7a03beaf0454bf6202a4f7e141b051a6ea
Tony Kay <twkay@thenavisway.com>
2015-11-02 14:40:26 -0800

    next version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject untangled-spec "0.1.1"
+(defproject untangled-spec "0.1.2-SNAPSHOT"
   :description "A Behavioral specification system for clj and cljs stacked on clojure.test"
   :url ""
   :license {:name "MIT Public License"
