9781aed91d94ee9a6e4157044a7f81eff3e960d2
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2015-12-29 10:27:15 -0800

    Story ID: RPC-236 Summary: renamed aliases to test-*

--- project.clj
+++ project.clj
@@ -49,6 +49,6 @@
                    :env          {:dev true}
                    }}
   :test-refresh {:report untangled-spec.reporters.terminal/untangled-report}
-  :aliases {"client" ["figwheel"]
-            "server" ["test-refresh"]}
+  :aliases {"test-client" ["figwheel"]
+            "test-server" ["test-refresh"]}
   )
