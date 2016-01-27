28fb8c275af457520f51d7f782d504718379fa7b
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2016-01-26 18:07:54 -0800

    Story ID: develop Summary: Fixed weird bug with specs not having the correct assert-expr loaded when they were compiled due to the fact that suite was compiled after *spec files. Fixed it by adding a require to suite macros in core.cljs after verifying my theory by turning on :verbose true and seeing the order *spec files and suite files were compiled in.

--- src/untangled_spec/core.cljs
+++ src/untangled_spec/core.cljs
@@ -1,4 +1,5 @@
 (ns untangled-spec.core
   (:require [untangled-spec.async]
             [untangled-spec.stub]
-            [cljs.test :include-macros true]))
+            [cljs.test :include-macros true]
+            [untangled-spec.reporters.suite :include-macros true]))
