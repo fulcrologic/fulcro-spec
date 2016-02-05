1a4a9d64742e0c8b66de82aca5592bd2a712ac58
Mark A. Walters <mawalters@thenavisway.com>
2016-02-05 13:08:33 -0800

    Navis repos refactored, changed resolving repo refs, and publishing refs.

--- project.clj
+++ project.clj
@@ -18,14 +18,12 @@
             [lein-doo "0.1.6"] ; for cljs CI tests
             [lein-figwheel "0.5.0-2" :exclusions [ring/ring-core commons-fileupload clj-time joda-time org.clojure/clojure org.clojure/tools.reader]]]
 
-  :repositories [["releases" "https://artifacts.buehner-fry.com/artifactory/internal-release"]
-                 ["third-party" "https://artifacts.buehner-fry.com/artifactory/internal-3rdparty"]
-                 ["snapshots" "https://artifacts.buehner-fry.com/artifactory/internal-snapshots"]]
+  :repositories [["releases" "https://artifacts.buehner-fry.com/artifactory/release"]]
 
-  :deploy-repositories [["releases" {:url           "https://artifacts.buehner-fry.com/artifactory/internal-release"
+  :deploy-repositories [["releases" {:url           "https://artifacts.buehner-fry.com/artifactory/navis-maven-release"
                                      :snapshots     false
                                      :sign-releases false}]
-                        ["snapshots" {:url           "https://artifacts.buehner-fry.com/artifactory/internal-snapshots"
+                        ["snapshots" {:url           "https://artifacts.buehner-fry.com/artifactory/navis-maven-snapshot"
                                       :sign-releases false}]]
 
   :source-paths ["src"]
