7e1c19d6b4991a73a6937a15d4dc828be53aac7b
Tony Kay <tony.kay@gmail.com>
2017-07-22 15:26:30 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro-spec "1.0.0-beta5-SNAPSHOT"
+(defproject fulcrologic/fulcro-spec "1.0.0-beta5"
   :description "A Behavioral specification system for clj and cljs stacked on clojure.test"
   :url ""
   :license {:name "MIT Public License"
