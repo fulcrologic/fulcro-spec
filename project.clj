04c241ffb1ea739a5a4958d4b3509a3fac51b143
Tony Kay <tony.kay@gmail.com>
2017-08-23 08:49:30 -0700

    fixed client render to not add websocket networking

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro-spec "1.0.0-beta8"
+(defproject fulcrologic/fulcro-spec "1.0.0-beta9-SNAPSHOT"
   :description "A Behavioral specification system for clj and cljs stacked on clojure.test"
   :url ""
   :license {:name "MIT Public License"
@@ -9,15 +9,15 @@
                  [kibu/pushy "0.3.7"]
                  [lein-doo "0.1.7" :scope "test"]
                  [ring/ring "1.6.2" :exclusions [commons-codec]]
-                 [fulcrologic/fulcro "1.0.0-beta7" :exclusions [org.clojure/clojure]]
+                 [fulcrologic/fulcro "1.0.0-beta8" :exclusions [org.clojure/clojure]]
                  [org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.854"]
+                 [org.clojure/clojurescript "1.9.908"]
                  [org.clojure/tools.namespace "0.3.0-alpha4"]
                  [clojure-future-spec "1.9.0-alpha17"]
                  [org.omcljs/om "1.0.0-beta1"]]
 
   :plugins [[com.jakemccrary/lein-test-refresh "0.19.0" :exclusions [org.clojure/tools.namespace]]
-            [lein-cljsbuild "1.1.6"]
+            [lein-cljsbuild "1.1.7"]
             [lein-doo "0.1.7"]                              ;; for cljs CI tests
             [lein-shell "0.5.0"]]
 
@@ -78,6 +78,6 @@
                                         :port             7007
                                         :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                          :dependencies [[com.cemerick/piggieback "0.2.2"]
-                                        [figwheel-sidecar "0.5.12" :exclusions [ring/ring-core http-kit joda-time]]
+                                        [figwheel-sidecar "0.5.13" :exclusions [ring/ring-core http-kit joda-time]]
                                         [org.clojure/tools.nrepl "0.2.13"]
                                         [org.clojure/test.check "0.9.0"]]}})
