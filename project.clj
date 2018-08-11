b1fd7053f949f256e4c33800cf3a5638f177e532
Tony Kay <tony.kay@gmail.com>
2018-08-11 12:36:08 -0700

    release
    Took 1 minute

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro-spec "2.1.0-SNAPSHOT"
+(defproject fulcrologic/fulcro-spec "2.1.0-2"
   :description "A Behavioral specification system for clj and cljs stacked on clojure.test"
   :url "https://github.com/fulcrologic/fulcro-spec"
   :license {:name "MIT Public License"
