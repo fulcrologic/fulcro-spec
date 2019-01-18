e0d434ce31db1c6954aa3edbb6631901082fcea8
Tony Kay <tony.kay@gmail.com>
2019-01-17 21:23:37 -0800

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro-spec "3.0.0-SNAPSHOT"
+(defproject fulcrologic/fulcro-spec "3.0.0"
   :description "Helper Macros for clj and cljs test"
   :url "https://github.com/fulcrologic/fulcro-spec"
   :license {:name "MIT Public License"
