a9c07edaf39fe64e904fd6b922c39fa93b4b73ba
Tony Kay <tony.kay@gmail.com>
2019-01-17 19:35:50 -0800

    renamed files

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/fulcrologic/fulcro-spec"
   :license {:name "MIT Public License"
             :url  "https://opensource.org/licenses/MIT"}
-  :dependencies [[colorize "0.1.1" :exclusions [org.clojure/clojure]]
-                 [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
-                 [fulcrologic/fulcro "2.6.7"]
-                 [org.clojure/clojure "1.10.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.439" :scope "provided"]]
+  :plugins [[lein-tools-deps "0.4.1"]]
 
-  :source-paths ["src"]
-  :test-paths ["test"])
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+
+  :jar-exclusions [#"public/.*" #"^workspaces/.*" #"\.DS_Store"]
+  :source-paths ["src/main"]
+  :test-paths ["src/test"])
